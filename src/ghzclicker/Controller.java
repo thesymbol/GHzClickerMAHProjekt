3107e240319bdf10887eadadc0faf3b3b6d19bbf
Mister Cat <Mister Cat@MisterCat-Dator>
2014-04-03 21:24:34 +0200

    Yes this is dog

---
+++ src/ghzclicker/Controller.java
@@ -0,0 +1,16 @@
+package ghzclicker;
+
+public class Controller {
+	private long hertz=0;
+	private long kiloHertz=0;
+	private long megaHertz=0;	
+	
+	/*
+	 * 
+	 */
+	public long GIVEMEYOURHERTZ(){
+		hertz++;
+		return hertz;
+	}
+	
+}
