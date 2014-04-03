42c2d3e43b11082aaa662681ac0952acb4c5e124
Matte <RH_Monztor@hotmail.com>
2014-04-03 22:14:34 +0200

    Added giga,tera,peta and exa hertz

--- src/ghzclicker/Controller.java
+++ src/ghzclicker/Controller.java
@@ -3,7 +3,11 @@ package ghzclicker;
 public class Controller {
 	private long hertz=0;
 	private long kiloHertz=0;
-	private long megaHertz=0;	
+	private long megaHertz=0;
+	private long gigaHertz=0;
+	private long teraHertz=0;
+	private long petaHertz=0;
+	private long exaHertz=0;
 	
 	/*
 	 * 
