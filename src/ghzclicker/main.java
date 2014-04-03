bdbc7115ae062a2f69bb511d9a091016f74ccee4
Marcus <marcus.orwen@gmail.com>
2014-04-03 20:47:53 +0200

    Test conflict

--- src/ghzclicker/main.java
+++ src/ghzclicker/main.java
@@ -2,6 +2,8 @@ package ghzclicker;
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("Jag är cool 2.0 asdasd");
+		System.out.println("Jag är cool 1.0");
+		System.out.println("Jag är cool 2.0");
+		System.out.println("Jag är cool 3.0");
 	}
 }
