a9bfc24a83ea3f7da751f94e82e3417fb45557c7
Mister Cat <Mister Cat@MisterCat-Dator>
2014-04-03 20:46:48 +0200

    Asd

--- src/ghzclicker/main.java
+++ src/ghzclicker/main.java
@@ -2,6 +2,7 @@ package ghzclicker;
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("Jag är cool 2.0 asdasd");
+		System.out.println("Jag är cool 2.0 asdasdasd");
+		
 	}
 }
