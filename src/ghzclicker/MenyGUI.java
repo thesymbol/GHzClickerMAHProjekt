27eba6e6243f9523eebb01b77d57d3255c4ea071
Matte <RH_Monztor@hotmail.com>
2014-04-09 00:38:42 +0200

    Added some building options, check if ok!

--- src/ghzclicker/MenyGUI.java
+++ src/ghzclicker/MenyGUI.java
@@ -9,8 +9,17 @@ import javax.swing.*;
 public class MenyGUI extends JPanel implements ActionListener {
 	private String hertz;
 	private Controller controller;
+	
+	//Making buttons with buildings and hertz button.
 	private JButton btnHertz = new JButton("hertz");
+	private JButton btnRam = new JButton("RAM Minne");
+	private JButton btnGraficCard = new JButton("Grafic card");
+	private JButton btnProcessor = new JButton("Processor");
+	private JButton btnHarddrive = new JButton("Hard drive");
+	private JButton btnMotherBoard = new JButton("MotherBoard");
 	private JLabel lblText = new JLabel("");
+
+	
 	
 	
 	public MenyGUI(Controller controller){
@@ -21,12 +30,23 @@ public class MenyGUI extends JPanel implements ActionListener {
 		setPreferredSize(new Dimension(800,800));
 		setLayout(null);
 		
-		//setting bounds.
+		//setting locations and size.
+		
 		lblText.setBounds(50, 50, 200, 50);
 		btnHertz.setBounds(50, 100, 200, 50);
+		btnRam.setBounds(600, 0, 200, 75);
+		btnGraficCard.setBounds(600, 75, 200, 75);
+		btnProcessor.setBounds(600, 150, 200, 75);
+		btnHarddrive.setBounds(600 , 225, 200, 75);
+		btnMotherBoard.setBounds(600, 300 , 200, 75);
 		
 		//adding the button and label to the frame.
 		add(btnHertz);
+		add(btnRam);
+		add(btnGraficCard);
+		add(btnProcessor);
+		add(btnHarddrive);
+		add(btnMotherBoard);
 		add(lblText);
 		
 		btnHertz.addActionListener(this);
