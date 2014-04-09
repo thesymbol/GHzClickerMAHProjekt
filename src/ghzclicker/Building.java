dd705f582316ae5453b4ec213269183399f35a48
Marcus <marcus.orwen@gmail.com>
2014-04-09 19:19:32 +0200

    Updated Building class with imageLocation for storing where the buildings image is located.

--- src/ghzclicker/Building.java
+++ src/ghzclicker/Building.java
@@ -6,9 +6,10 @@ package ghzclicker;
  * @author Marcus Orwén
  */
 public class Building {
-	private String name; 		// name of building
-	private int baseCost;		// base cost that will later be calculated with controller
-	private double baseHPS;		// base "hertz" per second later will be calculated with modifier
+	private String name; 			// name of building
+	private int baseCost;			// base cost that will later be calculated with controller
+	private double baseHPS;			// base "hertz" per second later will be calculated with modifier
+	private String imageLocation;	// building's image location for button.
 	
 	/**
 	 * Construct a building
@@ -16,11 +17,13 @@ public class Building {
 	 * @param name Name of building
 	 * @param cost Base cost of building
 	 * @param baseHPS base "Hertz" per Second
+	 * @param image location for building's button
 	 */
-	public Building(String name, int baseCost, double baseHPS) {
+	public Building(String name, int baseCost, double baseHPS, String imageLocation) {
 		this.name = name;
 		this.baseCost = baseCost;
 		this.baseHPS = baseHPS;
+		this.imageLocation = imageLocation;
 	}
 	
 	/**
@@ -48,5 +51,14 @@ public class Building {
 	 */
 	public double getBaseHPS() {
 		return baseHPS;
+	}
+	
+	/**
+	 * Get location of image for the button
+	 * 
+	 * @return the location of the image
+	 */
+	public String getImageLocation() {
+		return imageLocation;
 	}
 }
