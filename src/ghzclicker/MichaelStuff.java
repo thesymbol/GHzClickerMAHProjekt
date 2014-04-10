d504f29d7b88c49e1e222bcf064c8c3051672bef
Mister Cat <Mister Cat@MisterCat-Dator>
2014-04-10 21:58:08 +0200

    NU kan ni köra programet igen med en spel loop

--- src/ghzclicker/MichaelStuff.java
+++ src/ghzclicker/MichaelStuff.java
@@ -2,7 +2,7 @@ package ghzclicker;
 
 import java.util.Timer;
 
-public class MichaelStuff {
+public class MichaelStuff extends java.util.TimerTask {
 	private Controller controller;
 	private MenuGUI GUI;
 	private String hertz;
@@ -16,42 +16,41 @@ public class MichaelStuff {
 	public MichaelStuff(Controller controller, MenuGUI GUI){
 		this.controller=controller;
 		this.GUI=GUI;
-
+		
 	}
-
-//	public void run(){
-//		while(true){
-//			controller.merging();
-//			hertz=controller.stringiFy();
-//
-//
-//			GUI.uppdate(hertz);		
-//
-//		}
-//	}
 	
 	
-
-	public void gameLoop()
-	{
-	    timer = new Timer();
-	    timer.schedule(new LoopyStuff(), 0, 1000 / 60); //new timer at 60 fps, the timing mechanism
-	}
-
-	private class LoopyStuff extends java.util.TimerTask
-	{
-	    public void run() //this becomes the loop
-	    {
-	    	controller.merging();
+	
+	public void run(){
+		while(true){
+			controller.merging();
 			hertz=controller.stringiFy();
 
 
-			GUI.uppdate(hertz);	
+			GUI.uppdate(hertz);		
 
-	        if (!isRunning)
-	        {
-	            timer.cancel();
-	        }
-	    }
-	}
+		}
+	}	
+	 
+//	public void gameLoop(){
+//		timer = new Timer();
+//		timer.schedule(new LoopyStuff(), 0, 1000 / 60); //new timer at 60 fps, the timing mechanism
+//		
+//	}  
+//
+//	private class LoopyStuff extends java.util.TimerTask{
+//		
+//		public void run(){ //this becomes the loop
+//		
+//	    	controller.merging();
+//			hertz=controller.stringiFy();
+//
+//
+//			GUI.uppdate(hertz);	
+//
+//	        if (!isRunning){
+//	            timer.cancel();
+//	        }
+//	    }
+//	}
 }
