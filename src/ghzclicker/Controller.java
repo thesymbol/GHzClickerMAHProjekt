807f7ea78501d3346cea75194871c4020275cd72
Matte <RH_Monztor@hotmail.com>
2014-04-22 12:06:39 +0200

    Statistics still not working.

--- src/ghzclicker/Controller.java
+++ src/ghzclicker/Controller.java
@@ -147,19 +147,20 @@ public class Controller {
 	 * this will update the statistics all the time.
 	 */
 	public void uppdateStatistics() {
-
-		for (int i = 0; i < buildings.size(); i++) {
-			statistics += buildings.get(i).getOwned() + "\n Total Harddrives : "
-					+ buildings.get(0).getOwned() + "\n Total RAM : "
-					+ buildings.get(1).getOwned() + "\n Total PowerSupplies : "
-					+ buildings.get(2).getOwned() + "\n Total Harddrives(SSD) : "
-					+ buildings.get(3).getOwned() + "\n Total Graphics Cards : "
-					+ buildings.get(4).getOwned() + "\n Total Processors : "
-					+ buildings.get(5).getOwned() + "\n Total Motherboards : "
-					+ buildings.get(6).getOwned();
-		}
-		gui.updateStatistics(statistics);
-	}
+		  statistics="";
+		  for (int i = 0; i < buildings.size(); i++) {
+		   statistics += buildings.get(i).getOwned();
+		  }
+		  statistics+= buildings.get(0).getOwned() + "\n Total RAM : ";
+		  statistics+= buildings.get(1).getOwned() + "\n Total PowerSupplies : ";
+		  statistics+= buildings.get(2).getOwned() + "\n Total Harddrives(SSD) : ";
+		  statistics+= buildings.get(3).getOwned() + "\n Total Graphics Cards : ";
+		  statistics+= buildings.get(4).getOwned() + "\n Total Processors : ";
+		  statistics+= buildings.get(5).getOwned() + "\n Total Motherboards : ";
+		  statistics+= buildings.get(6).getOwned();
+		  
+		  gui.updateStatistics(statistics);
+		 }
 
 	/**
 	 * Viktor testar Ser om jag kan spara spelet Ändra till rätt HDD på datorn, på mitt windows8 tillåts inte programmet att skapa och spara en fil på C:/
