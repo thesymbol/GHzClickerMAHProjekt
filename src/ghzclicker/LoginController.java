7ff405f90c71a98c0d847b452a259c60aeeb3dfb
Matte <RH_Monztor@hotmail.com>
2014-04-29 13:33:56 +0200

    Working on login butto(not working atm)

--- src/ghzclicker/LoginController.java
+++ src/ghzclicker/LoginController.java
@@ -3,6 +3,8 @@ package ghzclicker;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 public class LoginController {
 	private LoginGUI logGUI;
 	private LoginListener listener;
@@ -16,10 +18,17 @@ public class LoginController {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (e.getSource() == logGUI.getBtnRegister()) {
-//				RegisterGUI regGUI = new RegisterGUI(listener);
+				RegisterGUI regGUI = new RegisterGUI(listener);
 			}
 			if (e.getSource() == logGUI.getbtnLogin()) {
-			
+				network.sendData(username);
+				network.senData(password);
+				if(username && password == true){
+					
+				}
+				else{
+					JOptionPane.showMessageDialog(null, "Wrong username or password, please try again");
+				}
 			}
 			if(e.getSource() == logGUI.getBtnExit()){
 				System.exit(0);
