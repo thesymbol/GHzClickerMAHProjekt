e23d3c4773e624902e66463a80ca3942fb03417e
Marcus <marcus.orwen@gmail.com>
2014-04-30 10:16:52 +0200

    Server can now accept recieve the login information (username/password). time to handle the request to send to the arduino

--- src/ghzclicker/LoginController.java
+++ src/ghzclicker/LoginController.java
@@ -27,20 +27,22 @@ public class LoginController {
 			}
 			if (e.getSource() == logGUI.getbtnLogin()) {
 				try {
+					String username = logGUI.getUsername();
+					String password = logGUI.getPassword();
 					NetworkClient network = new NetworkClient(serverIp);
 					network.sendData("sendusername");// send this first to notify that we will send the username next
-					network.sendData("username");
+					network.sendData(username);
 					network.sendData("sendpassword");// send this first to notify that we will send the password next
-					network.sendData("password");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				if (username && password == true) {
-					JOptionPane.showMessageDialog(null, "Successfully logged in");
-					logGUI.setVisible(false);
-					logGUI.dispose();
-				} else {
-					JOptionPane.showMessageDialog(null, "Wrong username or password, please try again");
+					network.sendData(password);
+					/*if (username && password == true) {
+						JOptionPane.showMessageDialog(null, "Successfully logged in");
+						logGUI.setVisible(false);
+						logGUI.dispose();
+					} else {
+						JOptionPane.showMessageDialog(null, "Wrong username or password, please try again");
+					}*/
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
 			}
 			if (e.getSource() == logGUI.getBtnExit()) {
