d0bbe5220f8a0b9181d6e328cf0cd7cf75ccaa2f
Loserffs <Michael.bergstrand@hotmail.com>
2014-05-01 11:32:04 +0200

    asd

--- src/ghzclicker/LoginController.java
+++ src/ghzclicker/LoginController.java
@@ -10,21 +10,18 @@ import javax.swing.JOptionPane;
 public class LoginController {
 	private LoginGUI logGUI;
 	private LoginListener listener;
-<<<<<<< HEAD
+
 	private RegisterGUI regGUI;
-=======
+
 	private String serverIp;
->>>>>>> branch 'master' of ssh://git@github.com/thesymbol/GHzClickerMAHProjekt.git
 
 	public LoginController(String ip) {
 		listener = new LoginListener();
 		logGUI = new LoginGUI(listener);
-<<<<<<< HEAD
 		regGUI = new RegisterGUI(listener);
-=======
+
 		this.serverIp = ip;
 
->>>>>>> branch 'master' of ssh://git@github.com/thesymbol/GHzClickerMAHProjekt.git
 	}
 
 	private class LoginListener implements ActionListener {
@@ -35,40 +32,47 @@ public class LoginController {
 				RegisterGUI regGUI = new RegisterGUI(listener);
 			}
 			if (e.getSource() == logGUI.getbtnLogin()) {
-<<<<<<< HEAD
 
 				network.sendData(username);
 				network.senData(password);
-				if(username && password == true){
+				if (username && password == true) {
 					JOptionPane.showMessageDialog(null, "Successfully logged in");
 					logGUI.setVisible(false);
 					logGUI.dispose();
-				}
-				else{
-					JOptionPane.showMessageDialog(null, "Wrong username or password, please try again");
-=======
-				try {
-					String username = logGUI.getUsername();
-					String password = logGUI.getPassword();
-					NetworkClient network = new NetworkClient(serverIp);
-					network.sendData("sendlogininfo");// send this first to notify that we will send the username and password next
-					network.sendData(username);
-					network.sendData(password);
-					if (network.getData().equals("test") && network.getData().equals("test2")) {
-						JOptionPane.showMessageDialog(null, "Successfully logged in");
-						logGUI.setVisible(false);
-						logGUI.dispose();
-					} else {
-						JOptionPane.showMessageDialog(null, "Wrong username or password, please try again");
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Wrong username or password, please try again");
+
+					try {
+						String username = logGUI.getUsername();
+						String password = logGUI.getPassword();
+						NetworkClient network = new NetworkClient(serverIp);
+						network.sendData("sendlogininfo");// send this first to
+															// notify that we
+															// will send the
+															// username and
+															// password next
+						network.sendData(username);
+						network.sendData(password);
+						if (network.getData().equals("test")
+								&& network.getData().equals("test2")) {
+							JOptionPane.showMessageDialog(null,
+									"Successfully logged in");
+							logGUI.setVisible(false);
+							logGUI.dispose();
+						} else {
+							JOptionPane
+									.showMessageDialog(null,
+											"Wrong username or password, please try again");
+						}
+						network.close();
+					} catch (IOException ex) {
+						ex.printStackTrace();
 					}
-					network.close();
-				} catch (IOException ex) {
-					ex.printStackTrace();
->>>>>>> branch 'master' of ssh://git@github.com/thesymbol/GHzClickerMAHProjekt.git
 				}
-			}
-			if (e.getSource() == logGUI.getBtnExit()) {
-				System.exit(0);
+				if (e.getSource() == logGUI.getBtnExit()) {
+					System.exit(0);
+				}
 			}
 		}
 	}
