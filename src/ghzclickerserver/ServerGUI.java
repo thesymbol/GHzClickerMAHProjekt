d8d430455a84b1dc9eae493c506fcdb27f861fd5
Matte <RH_Monztor@hotmail.com>
2014-05-09 11:49:16 +0200

    Added javadocs to ServerGUI

--- src/ghzclickerserver/ServerGUI.java
+++ src/ghzclickerserver/ServerGUI.java
@@ -6,13 +6,20 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
-
+/**
+ * A GUI for the server to show the "logger", using a JFrame to build the GUI.
+ * 
+ * @author Mattias Holst
+ *
+ */
 public class ServerGUI extends JFrame {
     private static final long serialVersionUID = 1L;
     private JButton btnExit = new JButton("Exit");
     private JTextArea taLog = new JTextArea();
     
-    
+    /**
+     * Constructor which builds up the whole GUI with sizes etc.
+     */
     public ServerGUI(){
         setPreferredSize(new Dimension(800, 850));
         setName("ServerGUI");
@@ -30,7 +37,10 @@ public class ServerGUI extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
     }
-    
+    /**
+     * Getting the exit button.
+     * @return btnExit
+     */
     public JButton getBtnExit(){
         return btnExit;
     }
