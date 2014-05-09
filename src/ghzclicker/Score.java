97dfb0e6442b783a8475135ee5a6e20cc6d6f78e
saltifish <viktorsaltarski@live.se>
2014-05-09 17:15:52 +0200

    Skapade klassen Score som kommer att användas för att hålla highscore objekt(poäng och namn)

---
+++ src/ghzclicker/Score.java
@@ -0,0 +1,28 @@
+package ghzclicker;
+
+public class Score {
+	private int score;
+	private String name;
+	
+	public Score(int score, String name){
+		this.score = score;
+		this.name = name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
