76ab090e03a723b814deccd1b77a5a1d190dba87
Matte <RH_Monztor@hotmail.com>
2014-05-09 13:26:51 +0200

    suckz

--- src/ghzclickerserver/ServerGUI.java
+++ src/ghzclickerserver/ServerGUI.java
@@ -2,17 +2,10 @@ package ghzclickerserver;
 
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.File;
 
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.Timer;
 /**
  * A GUI for the server to show the "logger", using a JFrame to build the GUI.
  * 
@@ -21,12 +14,8 @@ import javax.swing.Timer;
  */
 public class ServerGUI extends JFrame {
     private static final long serialVersionUID = 1L;
-    private JButton btnBrowse = new JButton("Browse");
+    private JButton btnExit = new JButton("Exit");
     private JTextArea taLog = new JTextArea();
-    File file = null;
-    BufferedReader reader = null;
-
-    private Timer timer = null;
     
     /**
      * Constructor which builds up the whole GUI with sizes etc.
@@ -35,28 +24,24 @@ public class ServerGUI extends JFrame {
         setPreferredSize(new Dimension(800, 850));
         setName("ServerGUI");
         setLayout(null);
-        btnBrowse.setBounds(550 , 50 , 90  , 30);
-        taLog.setBounds(0, 1, 800 , 650);
-        add(btnBrowse);
+        btnExit.setBounds(0 , 750 , 800  , 62);
+        taLog.setBounds(0, 0, 800 , 750);
+        add(btnExit);
         taLog.setFont(new Font("Arail" , Font.BOLD , 12));
         add(taLog);
         taLog.setEditable(false);
-        
-       
         
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-
-
     }
     /**
      * Getting the exit button.
      * @return btnExit
      */
     public JButton getBtnExit(){
-        return btnBrowse;
+        return btnExit;
     }
 }
