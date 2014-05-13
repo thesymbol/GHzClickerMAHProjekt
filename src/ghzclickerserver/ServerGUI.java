e5a3ed953d074d2c59d41e65f2f85ac41710379d
Matte <RH_Monztor@hotmail.com>
2014-05-13 12:32:15 +0200

    Added a scrollpane and auto update to the serverGUI

--- src/ghzclickerserver/ServerGUI.java
+++ src/ghzclickerserver/ServerGUI.java
@@ -4,10 +4,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 
+
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
 
 /**
  * A GUI for the server to show the "logger", using a JFrame to build the GUI.
@@ -19,7 +22,7 @@ public class ServerGUI extends JFrame {
     private static final long serialVersionUID = 1L;
     private JButton btnExit = new JButton("Exit");
     private static JTextArea taLog = new JTextArea();
-    private JScrollPane sp = new JScrollPane(taLog);
+    private JScrollPane sp = new JScrollPane(taLog, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
     /**
      * Constructor which builds up the whole GUI with sizes etc.
@@ -29,12 +32,15 @@ public class ServerGUI extends JFrame {
         setName("ServerGUI");
         setLayout(null);
         btnExit.setBounds(0, 750, 800, 62);
-        taLog.setBounds(0, 0, 800, 750);
+        add(sp);
+        sp.setBounds(0, 0, 785, 750);
         add(btnExit);
         taLog.setFont(new Font("Arail", Font.BOLD, 12));
-        add(taLog);
-        taLog.setEditable(true);
-        add(sp);
+        DefaultCaret caret = (DefaultCaret)taLog.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+
+        taLog.setEditable(false);
+
         btnExit.addActionListener(listener);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
