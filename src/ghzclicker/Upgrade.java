9b9c7c4774f31f1f5167f7f82918f466daa599fa
Matte <RH_Monztor@hotmail.com>
2014-05-19 08:15:59 +0200

    Added a new class for upgrades.

---
+++ src/ghzclicker/Upgrade.java
@@ -0,0 +1,53 @@
+package ghzclicker;
+/**
+ * Describe a base for Upgrades.
+ * 
+ * @author Mattias Holst
+ *
+ */
+public class Upgrade {
+    private String name;
+    private double baseCost;
+    private double baseHPS;
+    private double price;
+    /**
+     * Construct an upgrade.
+     * 
+     * @param name The name of the upgrade. 
+     * @param baseCost The base cost of the upgrade.
+     * @param baseHPS the baseHPS of the upgrade.
+     */
+    public Upgrade(String name, double baseCost, double baseHPS){
+        this.name = name;
+        this.baseCost = baseCost;
+        this.baseHPS = baseHPS;
+    }
+    /**
+     * get the upgrade name
+     * @return name The name of the upgrade.
+     */
+    public String getName(){
+        return name;
+    }
+    /**
+     * get the BaseCost
+     * @return baseCost The basecost of the upgrade.
+     */
+    public double getBaseCost(){
+        return baseCost;
+    }
+    /**
+     * setting the price.
+     * @param price Inserted price
+     */
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    /**
+     * Getting the price.
+     * @return price The price of the upgrade.
+     */
+    public double getPrice(){
+        return price;
+    }
+}
