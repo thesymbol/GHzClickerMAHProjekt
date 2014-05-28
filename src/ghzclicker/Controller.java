efac12b26a2f7cec9ec298b4686e22362b905778
Loserffs <Michael.bergstrand@hotmail.com>
2014-05-28 11:44:38 +0200

    asdf

--- src/ghzclicker/Controller.java
+++ src/ghzclicker/Controller.java
@@ -11,7 +11,6 @@ import java.util.logging.Logger;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JOptionPane;
 
 /**
  * A class that controlls the whole program. Having all the logics within the application
