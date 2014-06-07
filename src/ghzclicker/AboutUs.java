ac8508e52dbb49e8d36f58b5fde2e7e5ce69cc1d
Loserffs <Michael.bergstrand@hotmail.com>
2014-06-07 09:46:23 +0200

    Small change

--- src/ghzclicker/AboutUs.java
+++ src/ghzclicker/AboutUs.java
@@ -10,44 +10,44 @@ public class AboutUs extends JPanel {
     private JPanel pnlAboutUs = new BGPanel("res/wallpaper.png");
     private ImageIcon iconBack = new ImageIcon("res/btnBack.png");
     private ImageIcon iconBackPressed = new ImageIcon("res/btnBackPressed.png");
-    private ImageIcon iconMichael =new ImageIcon("res/michael bild.png");
-    private ImageIcon iconMattias =new ImageIcon("res/Mattias bild.png");
-    private ImageIcon iconViktor =new ImageIcon("res/Viktor bild.png");
-    private ImageIcon iconMarcus =new ImageIcon("res/Marcus bild.png");
-    private ImageIcon iconWung =new ImageIcon("res/Wung bild.png");
-    private ImageIcon iconEhsan =new ImageIcon("res/Ehsan bild.png");
-    //the text
+    private ImageIcon iconMichael = new ImageIcon("res/michael bild.png");
+    private ImageIcon iconMattias = new ImageIcon("res/Mattias bild.png");
+    private ImageIcon iconViktor = new ImageIcon("res/Viktor bild.png");
+    private ImageIcon iconMarcus = new ImageIcon("res/Marcus bild.png");
+    private ImageIcon iconWung = new ImageIcon("res/Wung bild.png");
+    private ImageIcon iconEhsan = new ImageIcon("res/Ehsan bild.png");
+    // the text
     private JLabel lblMichael = new JLabel("Michael Bergstrand");
     private JLabel lblMattias = new JLabel("Mattias Holst");
-    private JLabel lblViktor= new JLabel("Viktor Saltarski");
+    private JLabel lblViktor = new JLabel("Viktor Saltarski");
     private JLabel lblMarcus = new JLabel("Marcus Orwen");
     private JLabel lblWung = new JLabel("Wung Lung Chung");
     private JLabel lblEhsan = new JLabel("Ehsan");
     private JLabel lblText = new JLabel("We are a bunch of students who decided to make a game for our first project at Malmö University.");
     private JLabel lblText2 = new JLabel("All pictures are made by Michael in paint. Support his dream to beacome a paintist at *Kickstartar.com/MichaelThePaintist* thx");
-    //the pictures
+    // the pictures
     private JLabel lblBildMichael = new JLabel(iconMichael);
     private JLabel lblBildMattias = new JLabel(iconMattias);
-    private JLabel lblBildViktor= new JLabel(iconViktor);
+    private JLabel lblBildViktor = new JLabel(iconViktor);
     private JLabel lblBildMarcus = new JLabel(iconMarcus);
     private JLabel lblBildWung = new JLabel(iconWung);
     private JLabel lblBildEhsan = new JLabel(iconEhsan);
-    
+
     private JButton btnBack = new JButton("");
-    
+
     /**
-     * Constructor which will build the AboutUs GUI 
-     *
-     * @param listener Adding ActionListeners to the buttons 
-     */    
-    public AboutUs(ActionListener listener){
+     * Constructor which will build the AboutUs GUI
+     * 
+     * @param listener Adding ActionListeners to the buttons
+     */
+    public AboutUs(ActionListener listener) {
         setBackground(Color.white);
         pnlAboutUs.setPreferredSize(new Dimension(800, 553));
         add(pnlAboutUs);
         pnlAboutUs.setBackground(Color.white);
         pnlAboutUs.setLayout(null);
-        lblText2.setForeground(Color.green); 
-        //The back button       
+        lblText2.setForeground(Color.green);
+        // The back button
         btnBack.setBounds(675, 10, 108, 60);
         btnBack.setIcon(iconBack);
         btnBack.setOpaque(false);
@@ -56,25 +56,24 @@ public class AboutUs extends JPanel {
         btnBack.setFocusPainted(false);
         btnBack.setPressedIcon(iconBackPressed);
         btnBack.addActionListener(listener);
-        //where the picture are placed
-        lblBildMichael.setBounds(50,100,120,150);
-        lblBildMattias.setBounds(280,100,120,150);
-        lblBildViktor.setBounds(510,100,120,150);
-        lblBildMarcus.setBounds(50,300,120,150);
-        lblBildWung.setBounds(280,300,120,150);
-        lblBildEhsan.setBounds(510,300,120,150);
-        //where the names and text are placed 
-        lblMichael.setBounds(50,250,200,50);
-        lblMattias.setBounds(280,250,200,50);
-        lblViktor.setBounds(510,250,200,50);
-        lblMarcus.setBounds(50,450,200,50);
-        lblWung.setBounds(280,450,200,50);
-        lblEhsan.setBounds(510,450,200,50);
-        lblText.setBounds(50,50,700,50);
-        lblText2.setBounds(50,470,1000,50);
-        
-        
-        //adds everything to the panel
+        // where the picture are placed
+        lblBildMichael.setBounds(50, 100, 120, 150);
+        lblBildMattias.setBounds(280, 100, 120, 150);
+        lblBildViktor.setBounds(510, 100, 120, 150);
+        lblBildMarcus.setBounds(50, 300, 120, 150);
+        lblBildWung.setBounds(280, 300, 120, 150);
+        lblBildEhsan.setBounds(510, 300, 120, 150);
+        // where the names and text are placed
+        lblMichael.setBounds(50, 250, 200, 50);
+        lblMattias.setBounds(280, 250, 200, 50);
+        lblViktor.setBounds(510, 250, 200, 50);
+        lblMarcus.setBounds(50, 450, 200, 50);
+        lblWung.setBounds(280, 450, 200, 50);
+        lblEhsan.setBounds(510, 450, 200, 50);
+        lblText.setBounds(50, 50, 700, 50);
+        lblText2.setBounds(50, 470, 1000, 50);
+
+        // adds everything to the panel
         pnlAboutUs.add(btnBack);
         pnlAboutUs.add(lblBildMichael);
         pnlAboutUs.add(lblBildMattias);
@@ -90,19 +89,15 @@ public class AboutUs extends JPanel {
         pnlAboutUs.add(lblEhsan);
         pnlAboutUs.add(lblText);
         pnlAboutUs.add(lblText2);
-        
-        
-        
     }
-    
+
     /**
      * Returns the back button
      * 
      * @return the back button
-     */    
+     */
     public JButton getBtnBack() {
         return btnBack;
     }
 
 }
-
